622012cf7efc0477ffd8eb03f1933289bf9241f4
taro-watanabe <yutaro@protonmail.com>
2021-06-16 17:23:14 +0900

    ciao!

--- AutoMLTest.py
+++ AutoMLTest.py
@@ -6,18 +6,8 @@ pd.options.display.width = 0
 h2o.init()
 h2o.connect()
 
-
-df_origin = pd.read_csv("heart.csv")
-print(df_origin)
-
-def split_ingest(csv, label):
-    if type(csv) != str:
-        raise Exception("csv title  must be a string!!")
-
-    if type(label) != str:
-        raise Exception("label column name must be a string!!")
-
-    df_origin = pd.read_csv(csv)
+def split_ingest(df):
+    df_origin = pd.read_csv(df['csv'], low_memory=False)
     print(f"loaded DataFrame: \n{df_origin.head()}")
 
     train = df_origin.sample(frac=0.8,random_state=1)
@@ -27,23 +17,37 @@ def split_ingest(csv, label):
     testh2o = h2o.H2OFrame(test)
 
     x = trainh2o.columns
-    y = label
+    print(x)
+    y = df['label']
     x = x.remove(y)
 
-    trainh2o[y] = trainh2o[y].asfactor()
-    testh2o[y] = testh2o[y].asfactor()
+    if df['type'] == "reg":
+        trainh2o[y] = trainh2o[y].asnumeric()
+        testh2o[y] = testh2o[y].asnumeric()
+    elif df['type'] == "cla":
+        trainh2o[y] = trainh2o[y].ascharacter()
+        testh2o[y] = testh2o[y].ascharacter()
+        trainh2o[y] = trainh2o[y].asfactor()
+        testh2o[y] = testh2o[y].asfactor()
 
     return trainh2o, testh2o, x, y
 
-print(split_ingest("healthcare-dataset-stroke-data.csv", "stroke"))
+def read_task_csv():
+    df = pd.DataFrame({"csv": ["vgsales_alias.csv"],
+                       "label": ["Global_Sales"],
+                       "type": ["reg"],
+                       "leader_model": ["vgsales_leader"]})
+
+    return df
+
+for idx, row in read_task_csv().iterrows():
+    train, test, x, y = split_ingest(row)
+    aml = H2OAutoML(max_models=20, seed=1, max_runtime_secs=86400)
+    aml.train(x=x, y=y, training_frame=train)
 
 
-train, test, x, y = split_ingest("healthcare-dataset-stroke-data.csv", "stroke")
-aml = H2OAutoML(max_models=20, seed=1)
-aml.train(x=x, y=y, training_frame=train)
+    h2o.save_model(aml.leader, path=str(row['leader_model']))
 
-lb = aml.leaderboard
-print(lb)
-print(aml.leader)
+
 
 
